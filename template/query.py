73259dad202fccd7255d82fac49f7d4d41376610
julia-fc <jfcalderon@ucdavis.edu>
2020-02-02 22:38:03 -0800

    added comments to the insert function and fixed the typo that was on like 8 in main

--- template/query.py
+++ template/query.py
@@ -32,54 +32,61 @@ class Query:
     def insert(self, *columns):
         #schema_encoding = '0' * self.table.num_columns
         schema_encoding = 0
-
         #newRecord = Record(RIDcount, key,  )
+        
+        #Look for a page to put your record
         i = 0
         while(self.table.pages[i].num_records == 512):
             if(self.table.pages[i].num_records == 512):
                 i = i + self.table.num_columns
-        current_page = i
+        current_page = i #why does this need to be 0? because thats what it stays as
+        #when current page is used later it is 0, or whatever i is rn.
+        
+        #indirection
         indirection = 0
-        self.table.pages[i].write(indirection)
+        self.table.pages[i].write(indirection) #place indirection
+        #check if we filled up a page with our insertion
         if (self.table.pages[i].num_records == 512):
             new_page = Page()
             self.table.pages.append(new_page)
-        i = i + 1
+        i = i + 1 #move to the next column of data
 
-
+        #RID
         RID = self.RIDcount
-        self.RIDcount = self.RIDcount + 1
+        self.RIDcount = self.RIDcount + 1 #increment number of records we have
         self.table.pages[i].write(RID)
         if (self.table.pages[i].num_records == 512):
             new_page = Page()
             self.table.pages.append(new_page)
-        i = i + 1
+        i = i + 1 #move to the next column of data
 
         #timeStamp = time()
         self.table.pages[i].write(0)
         if (self.table.pages[i].num_records == 512):
             new_page = Page()
             self.table.pages.append(new_page)
-        i = i + 1
+        i = i + 1 #move to the next column of data
 
+        #Schema Encoding
         self.table.pages[i].write(schema_encoding)
         if (self.table.pages[i].num_records == 512):
             new_page = Page()
             self.table.pages.append(new_page)
-        i = i + 1
+        i = i + 1 #move to the next column of data
 
+        #now we enter the data that the user provided.
         j = 0
-        for column in columns:
-
-                if(j == 0):
-                    key = column
-                self.table.pages[i].write(column)
-
-                if (self.table.pages[i].num_records == 512):
-                        new_page = Page()
-                        self.table.pages.append(new_page)
-                i = i + 1
-                j = j + 1
+        for column in columns: #for as long as there are items in the list
+            if(j == 0): #first item: meanning it should be the key
+                key = column
+            #add the item to a page
+            self.table.pages[i].write(column)
+            #if we filled up the page, then make another one!
+            if (self.table.pages[i].num_records == 512):
+                    new_page = Page()
+                    self.table.pages.append(new_page)
+            i = i + 1
+            j = j + 1
         self.index.keyToRID[key] = RID
         self.table.page_directory[RID] =(current_page,     (self.table.pages[current_page].num_records - 1) * 8       )
         #print(self.table.page_directory)
@@ -110,9 +117,9 @@ class Query:
 
 
         #print(key)
-        print(self.updateRID)
-        print(page, offset)
-        print(columns)
+        #print(self.updateRID)
+        #print(page, offset)
+        #print(columns)
 
 
         pass
