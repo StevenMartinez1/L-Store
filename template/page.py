9f7744a9afd88131a89d7aa694295d3f0c7706bf
julia-fc <56012766+julia-fc@users.noreply.github.com>
2020-02-02 20:46:02 -0800

    Trying to add the template folder so whe dont have issues when we pull
    planning on deleting anything outside of template folder accept for main and readme

---
+++ template/page.py
@@ -0,0 +1,31 @@
+from template.config import *
+
+
+class Page:
+
+    def __init__(self):
+        self.num_records = 0
+        self.data = bytearray(4096)
+
+    def has_capacity(self):
+        if (self.num_records < 512):
+            return true
+        else:
+            return false
+        pass
+
+    def write(self, value):
+        self.num_records += 1
+        offset = (self.num_records - 1) * 8
+
+        #self.table.page_directory[self.query.RID] =(,)
+
+        for k in range(0, 7):
+            eight_bits = 255
+            eight_bits = value & eight_bits
+            self.data[offset + 7 - k] = eight_bits
+            value = value >> 8
+
+
+        pass
+
