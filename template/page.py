0683994fad5ace95273f157196b0cec91cd93bdb
StevenMartinez1 <emartinezgonzalez@ucdavis.edu>
2020-02-04 14:59:15 -0800

    ReadMe

---
+++ template/page.py
@@ -0,0 +1,42 @@
+from template.config import *
+
+
+class Page:
+
+    def __init__(self):
+        self.num_records = 0
+        self.data = bytearray(4096)
+
+    def has_capacity(self):
+        if (self.num_records < 512):
+            return true
+        else:
+            return false
+        pass
+
+    def write(self, value):
+        self.num_records += 1
+        offset = (self.num_records - 1) * 8
+
+
+        for k in range(0, 7):
+            eight_bits = 255
+            if(value == None):
+                break
+            eight_bits = value & eight_bits
+            self.data[offset + 7 - k] = eight_bits
+            value = value >> 8
+
+
+        pass
+
+
+    def writeAtOffset(self, value, offset):
+        #self.num_records += 1
+
+        for k in range(0, 8):
+            eight_bits = 255
+            eight_bits = value & eight_bits
+            self.data[offset + 7 - k] = eight_bits
+            value = value >> 8
+        pass
---
+++ template/table.py
@@ -0,0 +1,60 @@
+from template.page import *
+from time import time
+from template.config import *
+
+
+INDIRECTION_COLUMN = 0
+RID_COLUMN = 1
+TIMESTAMP_COLUMN = 2
+SCHEMA_ENCODING_COLUMN = 3
+
+
+class Record:
+
+    def __init__(self, rid, key, columns):
+        self.rid = rid
+        self.key = key
+        self.columns = columns
+        #print(self.columns)
+        #self.timeStamp = time()
+        #RIDcount = RIDcount + 1
+    def __str__(self):
+        list = '['
+        for column in self.columns:
+            list = list + str(column) + ', '
+
+        list = list[0:len(list)-2]
+
+        list = list + ']'
+
+        return list
+
+class Table:
+
+    """
+    :param name: string         #Table name
+    :param num_columns: int     #Number of Columns: all columns are integer
+    :param key: int             #Index of table key in columns
+    """
+    def __init__(self, name, num_columns, key):
+        self.name = name
+        self.key = key
+        self.num_columns = num_columns + 4
+        self.page_directory = {}
+        self.tail_page_directory = {}
+
+
+        self.pages = []
+        self.tail_pages = []
+        for i in range(0, self.num_columns):
+            new_page = Page()
+            self.pages.append(new_page)
+        for i in range(0, self.num_columns):
+            new_page = Page()
+            self.tail_pages.append(new_page)
+
+        pass
+
+    def __merge(self):
+        pass
+ 
