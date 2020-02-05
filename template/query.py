5acf017bbdb1f8bfdefade9a7fba207fcbf5c41b
Lekko1994 <34764880+Lekko1994@users.noreply.github.com>
2020-02-04 20:44:42 -0800

    Add files via upload

--- template/query.py
+++ template/query.py
@@ -15,7 +15,6 @@ class Query:
         self.table = table
         self.index = Index(table)
         self.RIDcount = 1
-        #self.RIDcount_tail = pow(2,64) - 1
         self.tailRIDcount = 2 ** 64 - 1
         pass
 
@@ -75,8 +74,8 @@ class Query:
             self.table.pages.append(new_page)
         i = i + 1
 
-        #timeStamp = time()
-        self.table.pages[i].write(0)
+        timeStamp = time()
+        self.table.pages[i].write(int(timeStamp))
         if (self.table.pages[i].num_records == 512):
             new_page = Page()
             self.table.pages.append(new_page)
@@ -103,13 +102,6 @@ class Query:
         self.index.keyToRID[key] = RID
         self.table.page_directory[RID] =(current_page,     (self.table.pages[current_page].num_records - 1) * 8       )
 
-        """
-        self.currentRID = self.index.keyToRID[key]
-        (page, offset) = self.table.page_directory[self.currentRID]
-        # print out all the values in the table base pages
-        for l in range(0, self.table.num_columns):
-            print(self.readRecord(page + l ,offset), end =" ")
-        """
         pass
 
 
@@ -148,15 +140,10 @@ class Query:
                 if(query_columns[j] == 0):
                     recordValues.append(None)
                 else:
-                    #print(recordIndirection)
                     (tailPage, tailOffset) = self.table.tail_page_directory[recordIndirection]
 
-                    #self.table.tail_page_directory[tailRID]
-
-                    #print('tailRID', recordIndirection)
-                    #print('tailPage and offset', tailPage, offset)
                     recordValues.append(self.readTailRecord(tailPage + i, tailOffset))
-                    #print(self.readRecord(tailPage + i, tailOffset))
+
                 j = j + 1
 
             record = Record(RID,key, recordValues)
@@ -199,7 +186,7 @@ class Query:
             self.table.tail_pages.append(new_page)
         #
         # Need to change base page indirection and schema encoding
-
+        #
 
 
 
@@ -217,18 +204,17 @@ class Query:
 
         # base page indirection = newly created tail page record RID
         self.table.pages[page].writeAtOffset(tailRID, offset)
-        #print(self.readRecord(page, offset))
-        #print(tailRID)
 
-        # timeStamp = time()
-        self.table.tail_pages[i].write(0)
+        timeStamp = time()
+        self.table.tail_pages[i].write(int(timeStamp))
         if (self.table.tail_pages[i].num_records == 512):
             new_page = Page()
             self.table.tail_pages.append(new_page)
         i = i + 1
 
 
-        schema_encoding = 0
+        schema_encoding = self.readRecord(page + 3, offset)
+        schema_page_index = i
 
         self.table.tail_pages[i].write(schema_encoding)
         if (self.table.tail_pages[i].num_records == 512):
@@ -236,10 +222,13 @@ class Query:
             self.table.tail_pages.append(new_page)
         i = i + 1
 
+        change_in_schema = 0 # 000..000
         for column in columns:
                 if(column != None):
                         self.table.tail_pages[i].write(column)
+                        change_in_schema = 1
                 if(column == None):
+                    change_in_schema = change_in_schema << 1
                     if(tailIndirection == 0):
                         column_index = i % self.table.total_columns
                         basePageRecordColumn = self.readRecord(page + column_index, offset)
@@ -257,15 +246,12 @@ class Query:
                         self.table.tail_pages.append(new_page)
                 i = i + 1
 
-        #self.index.keyToRID[key] = RID
-        self.table.tail_page_directory[tailRID] = (current_page,     (self.table.tail_pages[current_page].num_records - 1) * 8       )
+        schema_encoding = schema_encoding | change_in_schema
+        self.table.tail_pages[schema_page_index].writeAtOffset(schema_encoding, (self.table.tail_pages[current_page].num_records - 1) * 8)
+        self.table.pages[page + 3].writeAtOffset(schema_encoding, offset)
 
+        self.table.tail_page_directory[tailRID] = (current_page, (self.table.tail_pages[current_page].num_records - 1) * 8)
 
-        # print out all the values in the table tail pages
-        #self.currentRID = self.index.keyToRID[key]
-        #(page, offset) = self.table.page_directory[self.currentRID]
-        #for l in range(0, self.table.num_columns):
-        #   print(self.readTailRecord(i - self.table.num_columns + l , (self.table.tail_pages[i-1].num_records * 8 - 8)), end =" ")
         pass
 
 # it only reads from base pages
